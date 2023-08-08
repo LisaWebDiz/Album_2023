92ee7742c49425907c51bf9a8e54cb4bb60c0eba
LisaWebDiz <99768368+LisaWebDiz@users.noreply.github.com>
2023-08-08 17:36:53 +0300

    Update serializer.py

--- TestTask/album/serializer.py
+++ TestTask/album/serializer.py
@@ -1,13 +1,11 @@
 from rest_framework import serializers
-from .models import Album, Photo, User, Category
-
-
-
+from .models import Album, Photo, Category
 
 
 class PhotoSerializer(serializers.ModelSerializer):
     image_file = serializers.SerializerMethodField()
     thumbnail = serializers.SerializerMethodField()
+    
     class Meta:
         model = Photo
         fields = ['image_file', 'album', 'category', 'photo_pub_date', 'thumbnail']
@@ -20,9 +18,6 @@ class PhotoSerializer(serializers.ModelSerializer):
 
 
 class AlbumSerializer(serializers.ModelSerializer):
-    # drf вывести property
-    # вставить сериализатор фотографий, чтобы они выводились в постман
-
     album_photos = PhotoSerializer(many=True, read_only=True)
 
     class Meta:
@@ -38,37 +33,8 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['cat_title', 'photo_category']
 
 
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['name', 'surname']
-
-
 class UserAlbumForeignKey(serializers.PrimaryKeyRelatedField):
-    """Показывать альбомы принадлежащие юзеру"""
 
     def get_queryset(self):
         user = self.context['request'].user
         return Album.objects.filter(user=user)
-
-
-
-# class CategorySerializer(serializers.ModelSerializer):
-#     picture = serializers.SerializerMethodField()
-#     thumbnail = serializers.SerializerMethodField()
-#     class Meta:
-#         model = Category
-#         fields = ('id', 'name', 'picture','thumbnail')
-#     def get_picture(self, obj):
-#         return obj.picture.url
-#     def get_thumbnail(self, obj):
-#         return obj.thumbnail150x150.url
-
-
-
-
-
-
-
-
-
