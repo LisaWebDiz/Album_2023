a5deee51ef1d6a9d7ef09726d98e35e343f90a95
LisaWebDiz <99768368+LisaWebDiz@users.noreply.github.com>
2023-08-08 16:59:46 +0300

    Update urls.py

--- TestTask/TestTask/urls.py
+++ TestTask/TestTask/urls.py
@@ -15,20 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include, re_path
 
 from rest_framework import permissions
-# from drf_yasg2.views import get_schema_view
-# from drf_yasg2 import openapi
+from rest_framework import routers
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from rest_framework import routers
-from django.contrib.auth import views as auth_views
-# from album.views import AlbumViewSet, PhotoViewSet
-# from rest_framework.schemas import get_schema_view
-# from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -43,51 +38,17 @@ schema_view = get_schema_view(
     permission_classes=[permissions.AllowAny],
 )
 
-
-
-# schema_view = get_schema_view(
-#     openapi.Info(
-#         title="TestTask",
-#         default_version='v1',
-#         description="Test description",
-#         terms_of_service="https://www.google.com/policies/terms/",
-#         contact=openapi.Contact(email="contact@snippets.local"),
-#         license=openapi.License(name="BSD License"),
-#     ),
-#     public=True,
-#     permission_classes=(permissions.AllowAny,),
-# )
-
-# class CustomSchemaView(SchemaView):
-#     generator_class = CustomSchemaGenerator
-#     renderer_classes = (CustomRenderer1, CustomRenderer2,)
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('album/', include('album.urls')),
     path('api/v1/', include('album.urls')),
-
-    # path('api/drf-auth', include('rest_framework.urls')),
     path('api/v1/drf-auth', include('rest_framework.urls')),
-
-
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
-
-    # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0)),
-
-
-
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
