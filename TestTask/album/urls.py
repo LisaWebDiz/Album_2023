3c6bb6825c58ce2c2873905cd9f88dc49fb338e5
LisaWebDiz <99768368+LisaWebDiz@users.noreply.github.com>
2023-08-09 15:36:46 +0300

    Update urls.py

--- TestTask/album/urls.py
+++ TestTask/album/urls.py
@@ -4,13 +4,13 @@ from album.views import *
 urlpatterns = [
     path('albums/', AlbumAPIList.as_view()),
     path('album/<int:pk>/', AlbumAPIUpdate.as_view()),
-    path('albumdelete/<int:pk>/', AlbumAPIDestroy.as_view()),
+    path('delete_album/<int:pk>/', AlbumAPIDestroy.as_view()),
 
     path('photos/', PhotoAPIList.as_view()),
     path('photo/<int:pk>/', PhotoAPIUpdate.as_view()),
-    path('photodelete/<int:pk>/', PhotoAPIDestroy.as_view()),
+    path('delete_photo/<int:pk>/', PhotoAPIDestroy.as_view()),
 
     path('categories/', CategoryAPIList.as_view()),
     path('category/<int:pk>/', CategoryAPIUpdate.as_view()),
-    path('categorydelete/<int:pk>/', CategoryAPIDestroy.as_view()),
+    path('delete_category/<int:pk>/', CategoryAPIDestroy.as_view()),
 ]
