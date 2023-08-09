3865e0d39261b6e6e1761b649f9e4e2cbbf890e2
LisaWebDiz <99768368+LisaWebDiz@users.noreply.github.com>
2023-08-09 21:00:13 +0300

    Update serializer.py

--- TestTask/album/serializer.py
+++ TestTask/album/serializer.py
@@ -5,16 +5,20 @@ from .models import Album, Photo, Category
 class PhotoSerializer(serializers.ModelSerializer):
     image_file = serializers.SerializerMethodField()
     thumbnail = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Photo
-        fields = ['image_file', 'album', 'category', 'photo_pub_date', 'thumbnail']
+        fields = ['image_file', 'album', 'category', 'photo_pub_date', 'thumbnail', 'user']
 
     def get_image_file(self, obj):
         return obj.image_file.url
 
     def get_thumbnail(self, obj):
         return obj.thumbnail.url
+
+    def get_queryset(self):
+        user = self.context['request'].user
+        return Photo.objects.filter(username=user)
 
 
 class AlbumSerializer(serializers.ModelSerializer):
@@ -28,6 +32,7 @@ class AlbumSerializer(serializers.ModelSerializer):
 
 class CategorySerializer(serializers.ModelSerializer):
     photo_category = PhotoSerializer(many=True, read_only=True)
+
     class Meta:
         model = Category
         fields = ['cat_title', 'photo_category']
