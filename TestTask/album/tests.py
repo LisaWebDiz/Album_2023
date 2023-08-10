cb79c8976cd8a792c7cfe27c9bfdc435d61b9381
LisaWebDiz <99768368+LisaWebDiz@users.noreply.github.com>
2023-08-10 21:47:45 +0300

    Update tests.py

--- TestTask/album/tests.py
+++ TestTask/album/tests.py
@@ -5,7 +5,7 @@ from mixer.backend.django import mixer
 from rest_framework import status
 from rest_framework.test import APIRequestFactory, force_authenticate
 
-from .views import AlbumAPIList
+from .views import AlbumAPI
 from album.models import Album
 
 
@@ -39,14 +39,14 @@ class TestAPIAlbum(TestCase):
         self.album = mixer.blend(Album, username=self.user)
 
     def test_get_list_quest(self):
-        view = AlbumAPIList.as_view()
+        view = AlbumAPI.as_view()
         request = self.factory.get('api/v1/albums/')
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
     def test_get_list_user(self):
-        view = AlbumAPIList.as_view()
+        view = AlbumAPI.as_view()
         request = self.factory.get('api/v1/albums/')
         force_authenticate(request, user=self.user)
         response = view(request)
@@ -54,20 +54,20 @@ class TestAPIAlbum(TestCase):
 
 
     def test_get_detail_user(self):
-        view = AlbumAPIList.as_view()
+        view = AlbumAPI.as_view()
         request = self.factory.get(f'api/v1/albums/{self.album.id}')
         force_authenticate(request, self.user)
         response = view(request, pk=self.album.pk)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_post_quest(self):
-        view = AlbumAPIList.as_view()
+        view = AlbumAPI.as_view()
         request = self.factory.post('api/v1/albums/', self.album.album_title, format='json')
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_post_user(self):
-        view = AlbumAPIList.as_view()
+        view = AlbumAPI.as_view()
         album = {'album_title': 'test_album'}
         request = self.factory.post('api/v1/albums/', album, format='json')
         force_authenticate(request, user=self.user)
@@ -75,7 +75,7 @@ class TestAPIAlbum(TestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_delete_quest(self):
-        view = AlbumAPIList.as_view()
+        view = AlbumAPI.as_view()
         request = self.factory.delete(f'api/v1/albums/{self.album.pk}')
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
