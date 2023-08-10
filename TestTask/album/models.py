0d738ad1f4f659129bce455d4ff2ca83f7842bb1
LisaWebDiz <99768368+LisaWebDiz@users.noreply.github.com>
2023-08-10 19:41:30 +0300

    Update models.py

--- TestTask/album/models.py
+++ TestTask/album/models.py
@@ -13,12 +13,12 @@ class Album(models.Model):
     description = models.TextField(blank=True, null=True, verbose_name='Описание альбома')
     album_pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания альбома')
     photos_quantity = models.PositiveIntegerField(default=0, verbose_name='Количество фотографий в альбоме')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, verbose_name='Автор альбома')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор альбома')
 
     @property
     def photos_quantity(self):
         return self.album_photos.count()
-    
+
     def __str__(self):
         return 'Альбом: ' + self.album_title
 
@@ -31,13 +31,14 @@ class Album(models.Model):
 
 
 class Photo(models.Model):
-    image_file = models.FileField(null=True, blank=True, upload_to='image/%Y/%m/%d', validators=[image_validator], verbose_name='Фотография')
+    image_file = models.FileField(upload_to='image/%Y/%m/%d', validators=[image_validator], verbose_name='Фотография')
     photo_pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     category = models.ManyToManyField('Category', verbose_name='Категория фотографии', related_name='photo_category')
     album = models.ForeignKey('Album', on_delete=models.CASCADE, verbose_name='Альбом фотографии', related_name='album_photos')
     thumbnail = ImageSpecField(source='image_file', processors=[ResizeToFit(width=150, height=150, upscale=None, mat_color=None, anchor='c')], format="PNG", options={'quality': 60})
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, verbose_name='Автор альбома', related_name='user_photos')
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор альбома', related_name='user_photos')
+
+
     def __str__(self):
         return 'Фотография: ' + str(self.pk)
 
