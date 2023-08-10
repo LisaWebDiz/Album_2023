2000bdac49f42a91e3ea9128e2bb1e4a5b490a45
LisaWebDiz <99768368+LisaWebDiz@users.noreply.github.com>
2023-08-10 20:31:12 +0300

    Update filters.py

--- TestTask/album/filters.py
+++ TestTask/album/filters.py
@@ -4,34 +4,20 @@ from django_filters import CharFilter, NumberFilter, DateTimeFilter
 from .models import Album, Photo
 
 class AlbumFilter(django_filters.FilterSet):
-    title = CharFilter(field_name='album_title', lookup_expr='icontains')
+    album_title = CharFilter(field_name='album_title', lookup_expr='icontains', label='Поиск в названии альбома')
 
-    album_pub_date = DateTimeFilter(field_name='album_pub_date', lookup_expr='exact')
-    album_pub_date_gte = DateTimeFilter(field_name='album_pub_date', lookup_expr='gte')
-    album_pub_date_lte = DateTimeFilter(field_name='album_pub_date', lookup_expr='lte')
-    album_pub_date_gt = DateTimeFilter(field_name='album_pub_date', lookup_expr='gt')
-    album_pub_date_lt = DateTimeFilter(field_name='album_pub_date', lookup_expr='lt')
-
-    photos_quantity = NumberFilter(field_name='photos_quantity', lookup_expr='exact')
-    photos_quantity_gte = NumberFilter(field_name='photos_quantity', lookup_expr='gte')
-    photos_quantity_lte = NumberFilter(field_name='photos_quantity', lookup_expr='lte')
-    photos_quantity_gt = NumberFilter(field_name='photos_quantity', lookup_expr='gt')
-    photos_quantity_lt = NumberFilter(field_name='photos_quantity', lookup_expr='lt')
+    album_pub_date_gte = DateTimeFilter(field_name='album_pub_date', lookup_expr='gte', label='Дата создания: точная или позже')
+    album_pub_date_lte = DateTimeFilter(field_name='album_pub_date', lookup_expr='lte', label='Дата создания: точная или раньше')
 
     class Meta:
         model = Album
-        fields = ['album_pub_date', 'photos_quantity']
+        fields = ['album_title', 'album_pub_date']
 
 
 class PhotoFilter(django_filters.FilterSet):
-    category = CharFilter(field_name='category', lookup_expr='icontains', label='Поиск по категории')
-
-    photo_pub_date = DateTimeFilter(field_name='photo_pub_date', lookup_expr='exact')
-    photo_pub_date_gte = DateTimeFilter(field_name='photo_pub_date', lookup_expr='gte')
-    photo_pub_date_lte = DateTimeFilter(field_name='photo_pub_date', lookup_expr='lte')
-    photo_pub_date_gt = DateTimeFilter(field_name='photo_pub_date', lookup_expr='gt')
-    photo_pub_date_lt = DateTimeFilter(field_name='photo_pub_date', lookup_expr='lt')
+    photo_pub_date_gte = DateTimeFilter(field_name='photo_pub_date', lookup_expr='gte', label='Дата создания: точная или позже')
+    photo_pub_date_lte = DateTimeFilter(field_name='photo_pub_date', lookup_expr='lte', label='Дата создания: точная или раньше')
 
     class Meta:
         model = Photo
-        fields = ['category', 'photo_pub_date']
+        fields = ['photo_pub_date']
