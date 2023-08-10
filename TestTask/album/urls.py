aa01c1f8fdc4882baa6368cf83090f585915278f
LisaWebDiz <99768368+LisaWebDiz@users.noreply.github.com>
2023-08-10 21:48:56 +0300

    Update urls.py

--- TestTask/album/urls.py
+++ TestTask/album/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from album.views import *
 
 urlpatterns = [
-    path('albums/', AlbumAPIList.as_view()),
-    path('album/<int:pk>/', AlbumAPIUpdate.as_view()),
-    path('delete_album/<int:pk>/', AlbumAPIDestroy.as_view()),
+    path('albums/', AlbumAPI.as_view(), name='albums_api_list'),
+    path('album/<int:pk>/', AlbumAPI.as_view()),
+    path('delete_album/<int:pk>/', AlbumAPI.as_view()),
 
-    path('photos/', PhotoAPIList.as_view()),
-    path('photo/<int:pk>/', PhotoAPIUpdate.as_view()),
-    path('delete_photo/<int:pk>/', PhotoAPIDestroy.as_view()),
+    path('photos/', PhotoAPI.as_view(), name='photos_api_list'),
+    path('photo/<int:pk>/', PhotoAPI.as_view()),
+    path('delete_photo/<int:pk>/', PhotoAPI.as_view()),
 
-    path('categories/', CategoryAPIList.as_view()),
-    path('category/<int:pk>/', CategoryAPIUpdate.as_view()),
-    path('delete_category/<int:pk>/', CategoryAPIDestroy.as_view()),
+    path('categories/', CategoryAPI.as_view()),
+    path('category/<int:pk>/', CategoryAPI.as_view()),
+    path('delete_category/<int:pk>/', CategoryAPI.as_view()),
 ]
