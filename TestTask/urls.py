534d7a864d2a81aa0e841c1c6df445cb2b8f7d26
LisaWebDiz <99768368+LisaWebDiz@users.noreply.github.com>
2023-08-11 12:16:58 +0300

    Update urls.py

--- TestTask/urls.py
+++ TestTask/urls.py
@@ -14,16 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path, include, re_path
+from django.conf.urls.static import static
 
 from rest_framework import permissions
-from rest_framework import routers
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+
 
 schema_view = get_schema_view(
     openapi.Info(
